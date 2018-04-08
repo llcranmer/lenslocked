33318b6d7959143b3b37a9b04cbec96a1093b489
kbabkin <kbabkin@mediamath.com>
2018-04-08 17:48:18 -0400

    implemented the gorilla mux router

--- main.go
+++ main.go
@@ -3,14 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>welcome to my awesome site</h1>")
 }
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, `<p>To get in touch, please send an email to <a href="mailto:some@mail.com">some@mail.com</a>.</p>`)
+}
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
 }
