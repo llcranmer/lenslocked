920ce647e5629114779e80524c1ef6ae59052bea
kbabkin <kbabkin@mediamath.com>
2018-06-08 18:58:47 -0400

    7.9 created static controller

--- main.go
+++ main.go
@@ -2,43 +2,21 @@ package main
 
 import (
 	"./controllers"
-	"./views"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-var (
-	homeView    *views.View
-	contactView *views.View
-)
-
-func home(w http.ResponseWriter, _ *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(homeView.Render(w, nil))
-}
-func contact(w http.ResponseWriter, _ *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(contactView.Render(w, nil))
-}
-
 func main() {
-	homeView = views.NewView("bootstrap", "views/home.gohtml")
-	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 
 	usersC := controllers.NewUsers()
+	staticC := controllers.NewStatic()
 
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", home).Methods("GET")
-	r.HandleFunc("/contact", contact).Methods("GET")
+	r.Handle("/", staticC.HomeView).Methods("GET")
+	r.Handle("/contact", staticC.ContactView).Methods("GET")
 	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 	http.ListenAndServe(":3000", r)
-}
-
-func must(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
--- views/veiw.go
+++ views/veiw.go
@@ -34,7 +34,14 @@ type View struct {
 
 // Render writes parsed date into resp writer
 func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	w.Header().Set("Content-Type", "text/html")
 	return v.Template.ExecuteTemplate(w, v.Layout, data)
+}
+
+func (v *View) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := v.Render(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func layoutFiles() []string {
