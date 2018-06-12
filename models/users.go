a2ef382db0824e8b924dcdd67d8656f066cff255
kbabkin <kbabkin@mediamath.com>
2018-06-11 23:08:51 -0400

    9.2 created userService to interact with user model.

--- models/users.go
+++ models/users.go
@@ -1,9 +1,48 @@
 package models
 
 import (
+	"errors"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
+
+var (
+	ErrorNotFound = errors.New("models: resource not found")
+)
+
+type UserService struct {
+	db *gorm.DB
+}
+
+func NewUserService(connectionString string) (*UserService, error) {
+	db, err := gorm.Open("postgres", connectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	return &UserService{
+		db: db,
+	}, nil
+}
+
+func (us UserService) ByID(id uint) (*User, error) {
+	var user User
+
+	err := us.db.Where("id = ?", id).First(&user).Error
+
+	switch err {
+	case nil:
+		return &user, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrorNotFound
+	default:
+		return nil, err
+	}
+}
+
+func (us UserService) Close() error {
+	return us.db.Close()
+}
 
 type User struct {
 	gorm.Model
